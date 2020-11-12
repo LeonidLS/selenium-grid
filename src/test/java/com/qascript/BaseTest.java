db063b5b1c6166f3693f63cd459fb29a16879cd6
LeonidLS <llsuschenko@gmail.com>
2020-11-12 19:22:52 +0200

    sel-grid: chrome & opera

---
+++ src/test/java/com/qascript/BaseTest.java
@@ -0,0 +1,46 @@
+package com.qascript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+    //Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<RemoteWebDriver>();
+
+    @BeforeMethod
+    @Parameters(value = {"browser"})
+    public void setup(String browser) throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("browserName", browser);
+        //Set the Hub url (Docker exposed hub URL)
+        driver.set(new RemoteWebDriver(new URL("http://192.168.99.100:4446/wd/hub"), desiredCapabilities));
+
+    }
+
+    public WebDriver getDriver() {
+        //Get driver from ThreadLocalMap
+        return driver.get();
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        getDriver().quit();
+    }
+
+    @AfterClass
+    void terminate() {
+        //Remove the ThreadLocalMap element
+        driver.remove();
+    }
+
+
+}
